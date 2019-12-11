a8cc065bc2292381b79696d2895f03681e38e6e4
Colby Allen <colbyallen012@gmail.com>
2019-12-11 11:09:24 -0700

    rearrange app to class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Vino Cellar</h1>
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      wines: []
+    }
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <h1>Vino Cellar</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
