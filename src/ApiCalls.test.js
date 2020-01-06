c5d89b035e53292c1dde3730e42ba923efaa0830
Colby Allen <colbyallen012@gmail.com>
2020-01-06 11:37:07 -0700

    setup apicalls test file, run first test

---
+++ src/ApiCalls.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {fetchWines, postWine, deleteWine} from './ApiCalls'
+
+describe('ApiCalls', () => {
+  let mockWine;
+
+  beforeEach(() => {
+    mockWine = {
+      vineyard: 'Fake Vineyard',
+      name: 'Fake Name',
+      color: 'Red',
+      type: 'fake',
+      year: 2020,
+      rating: 1 
+    }
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockWine)
+      })
+    })
+  })
+
+  it('should be called with the correct url', async () => {
+    const expected = 'https://the-vino-cellar.herokuapp.com/api/v1/vinos';
+    fetchWines();
+    expect(window.fetch).toHaveBeenCalledWith(expected)
+  })
+})
