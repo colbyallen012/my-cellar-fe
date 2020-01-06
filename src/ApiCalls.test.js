6c7e30403ad45eeb1c97de4c5d555072645c9511
Colby Allen <colbyallen012@gmail.com>
2020-01-06 11:50:35 -0700

    cleanup apiCalls test file

--- src/ApiCalls.test.js
+++ src/ApiCalls.test.js
@@ -2,41 +2,44 @@ import React from 'react';
 import {fetchWines, postWine, deleteWine} from './ApiCalls'
 
 describe('ApiCalls', () => {
-  let mockWine;
 
-  beforeEach(() => {
-    mockWine = {
-      vineyard: 'Fake Vineyard',
-      name: 'Fake Name',
-      color: 'Red',
-      type: 'fake',
-      year: 2020,
-      rating: 1 
-    }
+  describ('Get Wine', () => {
+    let mockWine;
 
-    window.fetch = jest.fn().mockImplementation(() => {
-      return Promise.resolve({
-        ok: true,
-        json: () => Promise.resolve(mockWine)
+    beforeEach(() => {
+      mockWine = {
+        vineyard: 'Fake Vineyard',
+        name: 'Fake Name',
+        color: 'Red',
+        type: 'fake',
+        year: 2020,
+        rating: 1 
+      }
+
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockWine)
+        })
       })
     })
-  })
 
-  it('should be called with the correct url', async () => {
-    const expected = 'https://the-vino-cellar.herokuapp.com/api/v1/vinos';
-    fetchWines();
-    expect(window.fetch).toHaveBeenCalledWith(expected)
-  })
+    it('should be called with the correct url', async () => {
+      const expected = 'https://the-vino-cellar.herokuapp.com/api/v1/vinos';
+      fetchWines();
+      expect(window.fetch).toHaveBeenCalledWith(expected)
+    })
 
-  it('should return a parsed response', async () => {
-    const result = await fetchWines();
-    expect(result).toEqual(mockWine)
-  })
+    it('should return a parsed response', async () => {
+      const result = await fetchWines();
+      expect(result).toEqual(mockWine)
+    })
 
-  it('should return an error message', async () => {
-    window.fetch = jest.fn().mockImplementation(() => {
-      return Promise.reject('Error Fetching Wine')
-    });
-    await expect(window.fetch()).rejects.toEqual('Error Fetching Wine');
+    it('should return an error message', async () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.reject('Error Fetching Wine')
+      });
+      await expect(window.fetch()).rejects.toEqual('Error Fetching Wine');
+    })
   })
 })
