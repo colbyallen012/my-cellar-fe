37ee15fedf37530c1fb59df49387e700e5de84b0
Colby Allen <colbyallen012@gmail.com>
2020-01-13 10:49:27 -0700

    test for handleSubmit

--- src/WineForm.test.js
+++ src/WineForm.test.js
@@ -23,4 +23,11 @@ describe('WineForm', () => {
     wrapper.instance().handleChange(mockEvent);
     expect(wrapper.state('vineyard')).toEqual(expected)
   })
+
+  it('should invoke postWine when handleSubmit is called', () => {
+    const currState = {vineyard: 'my vineyard', name: 'my wine', color: 'red', type: 'merlot', year: 1990, rating: 10}
+    wrapper.instance().setState(currState)
+    wrapper.instance().handleSubmit({preventDefault: jest.fn()})
+    expect(wrapper.state('vineyard')).toEqual(currState.vineyard)
+  })
 })
