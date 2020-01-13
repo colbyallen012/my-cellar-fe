d5447271d4abf9c5d7f0025ae892472ed464073a
Colby Allen <colbyallen012@gmail.com>
2020-01-13 11:55:29 -0700

    Updates

---
+++ precache-manifest.70e90836b501e351606f7cf958c0fa2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5baf42f44b0621433d8414724e0f3983",
+    "url": "/my-cellar-fe/index.html"
+  },
+  {
+    "revision": "b126e04779f029ece288",
+    "url": "/my-cellar-fe/static/css/main.d28dd003.chunk.css"
+  },
+  {
+    "revision": "4befe31ab27a60c156b6",
+    "url": "/my-cellar-fe/static/js/2.34cb35d8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-cellar-fe/static/js/2.34cb35d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b126e04779f029ece288",
+    "url": "/my-cellar-fe/static/js/main.7b61302b.chunk.js"
+  },
+  {
+    "revision": "acede89ef9bcb57a59d2",
+    "url": "/my-cellar-fe/static/js/runtime-main.c97c3b9f.js"
+  },
+  {
+    "revision": "a151d44593fe047e2ec7627632a19175",
+    "url": "/my-cellar-fe/static/media/background.a151d445.jpeg"
+  }
+]);
