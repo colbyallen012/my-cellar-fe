978f4bd38821aa70f2834418dd914d86a44dac35
Colby Allen <colbyallen012@gmail.com>
2020-01-13 10:45:02 -0700

    handlechange test complete

--- src/WineForm.test.js
+++ src/WineForm.test.js
@@ -16,4 +16,11 @@ describe('WineForm', () => {
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should change state when handleChange is called', () => {
+    const mockEvent = { target: { name: 'vineyard', value: 'Wine Name'} };
+    const expected = 'Wine Name';
+    wrapper.instance().handleChange(mockEvent);
+    expect(wrapper.state('vineyard')).toEqual(expected)
+  })
 })
